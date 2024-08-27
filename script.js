8bc505931ac9184cad517669ef5b58ede3edc93f
Ashwaq Ali <154123000+Azhwake@users.noreply.github.com>
2024-08-27 14:42:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+let grade = 85;
+if (grade>=90 && grade <= 100) {
+    console.log("you got an A grade");
+}else if (grade >=75 && grade <90) {
+    console.log("you got a B grade");
+}else if (grade >=60 && grade < 75) {
+    console.log("you got a C grade") 
+}else if (grade <60) {
+    console.log("yo got an FAILED");
+}
+
